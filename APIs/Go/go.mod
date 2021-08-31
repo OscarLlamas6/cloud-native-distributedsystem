7619c9c9f972e0ae4ecde03352ce692ce68f3d36
201709309 <josesantizo149@gmail.com>
2021-08-31 16:12:58 -0600

    [ADDED] Go API

---
+++ APIs/Go/go.mod
@@ -0,0 +1,33 @@
+module github.com/apigo
+
+go 1.17
+
+require (
+	github.com/btnguyen2k/consu/gjrc v0.1.1 // indirect
+	github.com/btnguyen2k/consu/olaf v0.1.3 // indirect
+	github.com/btnguyen2k/consu/reddo v0.1.4 // indirect
+	github.com/btnguyen2k/consu/semita v0.1.4 // indirect
+	github.com/btnguyen2k/gocosmos v0.1.4 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/githubnemo/CompileDaemon v1.3.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/klauspost/compress v1.9.5 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.11 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/radovskyb/watcher v1.0.7 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.7.1 // indirect
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
---
+++ APIs/Go/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"database/sql"
+	_"encoding/json"
+	"log"
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"context"
+	"time"
+	_"os"
+	
+	"github.com/gorilla/mux"
+    _"github.com/go-sql-driver/mysql"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/joho/godotenv"
+)
+
+type tweet struct {
+	Nombre    string `json:"Nombre"`
+	Comentario string `json:"Comentario"`
+	Fecha string `json:"Fecha"`
+	Hashtags string `json:"Hashtags"`
+	Upvotes int `json:"Upvotes"`
+	Downvotes int `json:"Downvotes"`
+}
+
+func conexionGoogleCloud()(conexion *sql.DB){
+
+	envs, err := godotenv.Read(".env")
+	if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+	Driver:=envs["DRIVER"]
+	Usuario:=envs["USUARIO"]
+	Contrasena:=envs["CONTRASENA"]
+	Nombre:=envs["NOMBRE"]
+	Ip:=envs["IP"]
+
+	conexion,err2:= sql.Open(Driver, Usuario+":"+Contrasena+"@tcp("+Ip+")/"+Nombre)
+	if err2 != nil{
+		panic(err.Error())
+	}
+	return conexion;
+}
+
+func createTweetGoogleCloud(newTweet tweet){
+	conexion :=  conexionGoogleCloud()
+	insertarRegistros, err := conexion.Prepare("INSERT INTO TWEET(nombre, comentario, fecha, hashtags, upvotes, downvotes) VALUES (?, ?, ?, ?, ?, ?)")
+
+	if err != nil{
+		panic(err.Error())
+	}
+
+	insertarRegistros.Exec(newTweet.Nombre, newTweet.Comentario, newTweet.Fecha, newTweet.Hashtags, newTweet.Upvotes, newTweet.Downvotes);
+}
+
+func loadEnv(){
+	err:= godotenv.Load(".env")
+	if err != nil{
+		panic(err.Error())
+	}
+}
+
+/*func conexionCosmosSql(){
+
+	driver := "gocosmos"
+	dsn := "AccountEndpoint=https://98f82489-0ee0-4-231-b9ee.documents.azure.com/;AccountKey=DJf7JDRY4WnlUetsuYgb3lwJlWQEOuRkYEQFv8kFN0rdHQUDXjU5YwxGqAaAOAb2zyv6w3OZImT9nHIWykkYKQ=="
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		panic(err)
+	}
+	
+
+	sql := `INSERT INTO SOPES1.TWEET (nombre, comentario, fecha, hashtags, upvotes, downvotes) VALUES("\"Santizo\"","\"Test de golang a cosmos\"","\"30/08/2021\"","\"hg1,hg2\"",50,60)`
+	result, err := db.Exec(sql,"mypk")
+	if err != nil {
+		panic(err)
+	}
+
+	numRows, err := result.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Number of rows affected:", numRows)
+
+	defer db.Close()
+	}*/
+
+
+func conexionCosmos()*mongo.Client{
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+    defer cancel()
+
+	envs, err := godotenv.Read(".env")
+	if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    cosmoskey := envs["COSMOSKEY"]
+    clientOptions := options.Client().ApplyURI(cosmoskey).SetDirect(true)
+    
+    c, err := mongo.NewClient(clientOptions)
+    err = c.Connect(ctx)
+    if err != nil {
+        log.Fatalf("unable to initialize connection %v", err)
+    }
+
+    err = c.Ping(ctx, nil)
+    if err != nil {
+        log.Fatalf("unable to connect %v", err)
+    }
+
+	return c;
+}
+
+
+func createTweetCosmos(newTweet tweet){
+	c := conexionCosmos()
+	ctx := context.Background()
+	defer c.Disconnect(ctx)
+
+	todoCollection := c.Database("SOPES1").Collection("TWEET")
+	r, err := todoCollection.InsertOne(ctx, newTweet)
+	if err != nil {
+		log.Fatalf("failed to add todo %v", err)
+	}
+	fmt.Println("added todo", r.InsertedID)
+}
+
+func crearTweet(w http.ResponseWriter, r *http.Request) {
+	//enableCors(&w)
+
+	var newTweet tweet
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Insert a Valid Client Data")
+	}
+
+	json.Unmarshal(reqBody, &newTweet)
+
+	createTweetCosmos(newTweet)
+	createTweetGoogleCloud(newTweet)
+	
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTweet)
+}
+
+
+
+
+
+func main() {
+	//conexionCosmosSql()
+    router := mux.NewRouter().StrictSlash(true)
+
+	//router.HandleFunc("/", indexRoute)
+	//router.HandleFunc("/tasks", createTask).Methods("POST")
+	router.HandleFunc("/tweet", crearTweet).Methods("POST")
+	//router.HandleFunc("/tasks/{id}", getOneTask).Methods("GET")
+	//router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+	//router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+
+	log.Fatal(http.ListenAndServe(":4000", router))
+}
