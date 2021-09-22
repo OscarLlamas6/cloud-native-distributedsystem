1ad4843d86b1df2fad4b7239d6e419ef2b4db63d
SergioEGGit <sergioeg19@hotmail.com>
2021-09-22 03:27:15 +0000

    [UPDATED] index.js - Server Kernel

--- APIs/modulos-kernel-centos/index.js
+++ APIs/modulos-kernel-centos/index.js
@@ -1,5 +1,6 @@
 const morgan = require('morgan');
 const express = require('express');
+const fs = require('fs');
 require('dotenv').config();
 
 var app = express();
@@ -11,6 +12,21 @@ app.get('/', (req, res)=>{
     res.status(200).send({ mensaje: "Servicio módulos kernel - SOPES1 :D ", })
 });
 
+app.get('/leer', (req, res)=>{ 
+
+    try {
+        const RAMdata = fs.readFileSync('/proc/rammodule', 'utf8');
+        const CPUdata = fs.readFileSync('/proc/processmodule', 'utf8');
+        console.log(RAMdata, CPUdata)
+        res.status(200).send({  RAM: RAMdata.toString(),
+                                CPU: CPUdata.toString(),
+                                mensaje: "Módulos kernel leidos exitosamente :D!" });
+      } catch (err) {
+        console.error(err);
+        res.status(404).send({ mensaje: "Error al leer módulos kernel :(" });
+      }
+
+});
 
 app.listen(port, () => {
     console.log(`Service is running at ${port} :D`)
---
+++ Módulos_Kernel/Módulo_Ram/rammodule_centos.c
@@ -0,0 +1,104 @@
+// Imports 
+
+// Inizialization
+#include <linux/init.h>
+
+// Functions
+#include <linux/module.h>
+
+// Processs File System Constants/Structures
+#include <linux/proc_fs.h>
+
+// Files Manage
+#include <linux/seq_file.h>
+
+// Memory Manage
+#include <linux/mm.h>
+
+// Cpu Proccess
+#include <linux/sched/signal.h>
+
+// Module Identification
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Grupo 18");
+MODULE_DESCRIPTION("Proyecto No.1 - Sistemas Operativos 1");
+MODULE_VERSION("1.0.0");
+
+// Write File, Params File Pointer, V Pointer
+int writeFile(struct seq_file* file, void *v) {
+     
+    // Declaratiosn 
+    long porcent;
+    long freeRam;
+    long useRam;
+    long totalRam;
+    
+    // Sys Information Struct
+    struct sysinfo information;
+    
+    // Obtain Sys Informartion
+    si_meminfo(&information);
+
+    // Porcent And Values
+    freeRam = (information.freeram * information.mem_unit) / (1000 * 1000);
+    totalRam = (information.totalram * information.mem_unit) / (1000 * 1000);
+    useRam = (totalRam - freeRam); 
+             
+    porcent = (useRam * 100 / totalRam); 
+
+    // Write In File Values 
+    seq_printf(file, "%8ld,", totalRam);
+    seq_printf(file, "%8ld,", useRam);   
+    seq_printf(file, "%8ld,", freeRam);
+    seq_printf(file, "%8ld", porcent);  
+  
+    // Return File 
+    return 0;
+
+}
+
+// Action When Opening The File
+static int atOpen(struct inode* inode, struct file* file) {
+
+    // Open File
+    return single_open(file, writeFile, NULL);
+
+}
+
+// File Operations
+static struct file_operations fileOperations = {
+
+    open: atOpen,
+    read: seq_read
+
+};
+
+// Load Module
+static int load_module(void) {
+
+    // Print Title
+    printk(KERN_INFO "mod: Bienvenido Al Módulo Kernel De RAM - Grupo 18\n");
+
+    // Create Proccess
+    proc_create("rammodule", 0, NULL, &fileOperations);
+    
+    // Return 
+    return 0;
+
+}
+
+static void unload_module(void) {
+    
+    // Print Title
+    printk(KERN_INFO "mod: Gracias Por Utilizar Nuestro Modulo Kernel De RAM - Grupo 18\n");
+
+    // Remove Proccess
+    remove_proc_entry("rammodule", NULL);
+
+}
+
+// Module Initialization
+module_init(load_module);
+
+// Module Exit
+module_exit(unload_module);
